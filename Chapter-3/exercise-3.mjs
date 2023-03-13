9d95393794359b4cc9ad834063bffc62cbe9035c
Mohamed Momen <m7medmo2men@gmail.com>
2023-03-13 21:02:11 +0200

    Solve Excersie 3 of Chapter 3.

---
+++ Chapter-3/exercise-3.mjs
@@ -0,0 +1,34 @@
+import { EventEmitter } from "events";
+
+class Ticker extends EventEmitter {
+  constructor(milliseconds, cb) {
+    super();
+    this.milliseconds = milliseconds;
+    this.cb = cb;
+    this.timePassed = 0;
+    this.cnt = 0;
+  }
+
+  repeat() {
+    setTimeout(() => {
+      if (this.timePassed >= this.milliseconds) {
+        return this.cb(this.cnt);
+      }
+      this.emit("tick", ++this.cnt);
+      this.timePassed += 50;
+      this.repeat();
+    }, 50);
+  }
+
+  start() {
+    process.nextTick(() => this.emit("functionStarted"));
+    this.repeat();
+    return this;
+  }
+}
+
+const ticker = new Ticker(200, (cnt) => console.log("ticker finished with total count: " + cnt));
+ticker
+  .start()
+  .on("tick", (cnt) => console.log(`tick ${cnt}`))
+  .on("functionStarted", () => console.log("function started"));
